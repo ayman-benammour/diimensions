94a11408eb097e98b835eff1b50e49bcdee6605e
Enas <enas.elmershati@gmail.com>
2024-06-26 19:48:10 +0200

    Add minecraft scene

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -13,7 +13,7 @@ import sources from './sources.js'
 let instance = null
 
 export default class Experience {
-    constructor(_canvas, onSceneChange) {
+    constructor(_canvas) {
         // Singleton
         if (instance) {
             return instance
@@ -25,7 +25,6 @@ export default class Experience {
 
         // Options
         this.canvas = _canvas
-        this.onSceneChange = onSceneChange
 
         // Setup
         this.debug = new Debug()
@@ -52,80 +51,12 @@ export default class Experience {
         this.time.on('tick', () => {
             this.update()
         })
-
-        // Initialize the first scene
-        this.startSceneSwitching()
-    }
-
-    startSceneSwitching() {
-        setInterval(() => {
-            this.sceneDisplay = this.sceneDisplay === 1 ? 2 : 1
-            this.onSceneChange(this.sceneDisplay)
-            this.switchScene()
-        }, 5000)
-    }
-
-    switchScene() {
-        this.destroyCurrentScene()
-        if (this.sceneDisplay === 1) {
-            this.loadScene1()
-        } else {
-            this.loadScene2()
-        }
-    }
-
-    loadScene1() {
-        // Code to initialize scene 1
-        console.log("Loading Scene 1")
-        // Add your specific scene 1 initialization code here
-    }
-
-    loadScene2() {
-        // Code to initialize scene 2
-        console.log("Loading Scene 2")
-        // Add your specific scene 2 initialization code here
-    }
-
-    destroyCurrentScene() {
-        // Traverse the scene and dispose of all geometries, materials, and textures
-        this.scene.traverse((child) => {
-            if (child instanceof THREE.Mesh) {
-                child.geometry.dispose()
-
-                if (Array.isArray(child.material)) {
-                    for (let material of child.material) {
-                        this.disposeMaterial(material)
-                    }
-                } else {
-                    this.disposeMaterial(child.material)
-                }
-            }
-        })
-
-        if (this.camera.controls) {
-            this.camera.controls.dispose()
-        }
-        this.renderer.instance.dispose()
-
-        if (this.debug.active)
-            this.debug.ui.destroy()
-    }
-
-    disposeMaterial(material) {
-        if (material) {
-            for (const key in material) {
-                if (material[key] && typeof material[key].dispose === 'function') {
-                    material[key].dispose()
-                }
-            }
-        }
     }
 
     resize() {
         this.camera.resize()
         this.renderer.resize()
     }
-
 
     update() {
         this.camera.update()
@@ -137,6 +68,28 @@ export default class Experience {
         this.sizes.off('resize')
         this.time.off('tick')
 
-        this.destroyCurrentScene()
+        // Traverse the whole scene
+        this.scene.traverse((child) => {
+            // Test if it's a mesh
+            if (child instanceof THREE.Mesh) {
+                child.geometry.dispose()
+
+                // Loop through the material properties
+                for (const key in child.material) {
+                    const value = child.material[key]
+
+                    // Test if there is a dispose function
+                    if (value && typeof value.dispose === 'function') {
+                        value.dispose()
+                    }
+                }
+            }
+        })
+
+        this.camera.controls.dispose()
+        this.renderer.instance.dispose()
+
+        if (this.debug.active)
+            this.debug.ui.destroy()
     }
 }
