c4da0a69c1f5d90c8e3f0ed3a167d19caf6d2aaa
Enas <enas.elmershati@gmail.com>
2024-06-26 16:23:22 +0200

    Add lego scene

---
+++ src/Lego/script.js
@@ -0,0 +1,70 @@
+import * as THREE from 'three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+export default class SceneApp {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(0xD1E0F9);
+
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera.position.z = 5;
+
+        this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+        this.controls = new OrbitControls(this.camera, this.canvas);
+        this.addLights();
+
+        this.loadModels();
+        this.addEventListeners();
+        this.animate();
+    }
+
+    addLights() {
+        const light = new THREE.AmbientLight(0xffffff, 5);
+        this.scene.add(light);
+    }
+
+    loadModels() {
+        const mtlLoader = new MTLLoader();
+        const objLoader = new OBJLoader();
+
+        mtlLoader.load('Lego/lego-material.mtl', (materials) => {
+            materials.preload();
+            objLoader.setMaterials(materials);
+            objLoader.load(
+                'Lego/lego.obj',
+                (object) => {
+                    this.scene.add(object);
+                    console.log('Model loaded successfully!');
+                },
+                (xhr) => {
+                    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+                },
+                (error) => {
+                    console.error('An error happened while loading the model: ', error);
+                }
+            );
+        });
+    }
+
+    animate() {
+        requestAnimationFrame(this.animate.bind(this));
+        this.controls.update();
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    addEventListeners() {
+        window.addEventListener('resize', this.onWindowResize.bind(this), false);
+    }
+
+    onWindowResize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+}
+
