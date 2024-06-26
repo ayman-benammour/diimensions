dd2ca71e746b8e56c89459d2529079c29fce342d
ayman-benammour <benammourayman@gmail.com>
2024-06-26 12:59:20 +0200

    :sparkles: Add character controls feature

--- src/Experience/Utils/CharacterControls.js
+++ src/Experience/Utils/CharacterControls.js
@@ -11,6 +11,7 @@ export default class CharacterControls
         this.time = this.experience.time
         this.debug = this.experience.debug
         this.model = this.experience.world.fox
+        this.camera = this.experience.camera
 
         // Debug
         if(this.debug.active)
@@ -25,8 +26,8 @@ export default class CharacterControls
         this.cameraTarget = new THREE.Vector3()
 
         // Constants
-        this.runVelocity = 5
-        this.walkVelocity = 2
+        this.runVelocity = 0.008
+        this.walkVelocity = 0.002
 
         // Keys
         this.zKey = 'z'
@@ -66,6 +67,45 @@ export default class CharacterControls
         },false)
     }
 
+    directionOffset(keysPressed) 
+    {
+        let directionOffset = 0 // Z
+
+        if (keysPressed[this.sKey]) {
+            if (keysPressed[this.dKey]) {
+                directionOffset = Math.PI / 4 // Z+Q
+            } else if (keysPressed[this.qKey]) {
+                directionOffset = - Math.PI / 4 // Z+D
+            }
+        } else if (keysPressed[this.zKey]) {
+            if (keysPressed[this.dKey]) {
+                directionOffset = Math.PI / 4 + Math.PI / 2 // S+Q
+            } else if (keysPressed[this.qKey]) {
+                directionOffset = -Math.PI / 4 - Math.PI / 2 // S+D
+            } else {
+                directionOffset = Math.PI // s
+            }
+        } else if (keysPressed[this.dKey]) {
+            directionOffset = Math.PI / 2 // Q
+        } else if (keysPressed[this.qKey]) {
+            directionOffset = - Math.PI / 2 // D
+        }
+
+        return directionOffset
+    }
+
+    updateCameraTarget(moveX, moveZ)
+    {
+        this.camera.instance.position.x -= moveX
+        this.camera.instance.position.z -= moveZ
+
+        // update camera target
+        this.cameraTarget.x = this.model.model.position.x
+        this.cameraTarget.y = this.model.model.position.y + 1
+        this.cameraTarget.z = this.model.model.position.z
+        this.camera.controls.target = this.cameraTarget
+    }
+
     update()
     {
 
@@ -83,6 +123,38 @@ export default class CharacterControls
         if (this.currentAction !== this.newAction) {
             this.currentAction = this.newAction
             this.model.animation.play(this.currentAction)
+        }
+
+        if (this.currentAction == 'running' || this.currentAction == 'walking') {
+
+            // Calculate towards camera direction
+            const angleYCameraDirection = Math.atan2(
+                (this.camera.instance.position.x - this.model.model.position.x),
+                (this.camera.instance.position.z - this.model.model.position.z)
+            )
+
+            // Diagonal movement angle offset
+            const directionOffset = this.directionOffset(this.keysPressed)
+
+            // Rotate model
+            this.rotateQuaternion.setFromAxisAngle(this.rotateAngle, angleYCameraDirection + directionOffset)
+            this.model.model.quaternion.rotateTowards(this.rotateQuaternion, 0.07)
+
+            // Calculate direction
+            this.camera.instance.getWorldDirection(this.walkDirection)
+            this.walkDirection.y = 0
+            this.walkDirection.normalize()
+            this.walkDirection.applyAxisAngle(this.rotateAngle, directionOffset)
+
+            // Run-walk velocity
+            const velocity = this.currentAction == 'running' ? this.runVelocity : this.walkVelocity
+
+            // Move model & camera
+            const moveX = this.walkDirection.x * velocity * this.time.delta
+            const moveZ = this.walkDirection.z * velocity * this.time.delta
+            this.model.model.position.x -= moveX
+            this.model.model.position.z -= moveZ
+            this.updateCameraTarget(moveX, moveZ)
         }
 
     }
