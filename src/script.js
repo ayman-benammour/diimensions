eb661891820cb44ba22f6991d3800329fc414560
ayman-benammour <benammourayman@gmail.com>
2024-06-26 20:52:35 +0200

    :lipstick: Add ui phone

--- src/script.js
+++ src/script.js
@@ -1,4 +1,9 @@
-
 import Experience from './Experience/Experience.js'
-
 const experience = new Experience(document.querySelector('canvas.webgl'))
+
+// Phone
+const uiPhoneElement = document.querySelector('.uiPhone')
+
+uiPhoneElement.addEventListener('click', () => {
+    uiPhoneElement.classList.toggle('active')
+})
